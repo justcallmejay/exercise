ed6855cde63153eaf81929d6d59da220d7dbadd7
jay <jaypifiverr@gmail.com>
2023-05-22 15:44:34 -0400

    Add View Progress Button

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './User.css'
 
-function User( { updateUser } ) {
+function User( { updateUser, userRoutine, setUserRoutine, currentUser } ) {
 
     const history = useHistory()
 
@@ -18,12 +18,36 @@ function User( { updateUser } ) {
         })
     }
 
+    useEffect(() => {
+        if (currentUser !== false) {
+        fetch(`/routines/?user_id=${currentUser.id}`)
+        .then(res => res.json())
+        .then(res => setUserRoutine(res))
+        }
+      }, [])
+
+
+    const hasOne = userRoutine.length > 0 ? true : false
+
+    console.log(currentUser)
+    console.log(userRoutine)
+    console.log(hasOne)
     return(
         <div className="user c-c">
                 <button onClick={handleLogout}>Logout</button>
             <div className='userpg-container c-c'>
-                <button className='userpg-btn'>Select Exercise Routine</button>
-                <button className='userpg-btn'>Create Exercise Routine</button>
+                <Link to='/start-exercise'>
+                    <button className='userpg-btn hm-btn'>Start Exercise</button>
+                </Link>
+                <Link to='/select-routine'>
+                    <button className={`userpg-btn ${hasOne ? "" : "not-allowed"}`}>{hasOne ? 'Select Exercise Routine' : 'Start Below'}</button>
+                </Link>
+                <Link to='/create-new'>
+                    <button className='userpg-btn hm-btn'>Create Exercise Routine</button>
+                </Link>
+                <Link to='/progress'>
+                    <button className='userpg-btn hm-btn'>View Progression</button>
+                </Link>
             </div>
         </div>
     )
