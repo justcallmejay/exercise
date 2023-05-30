729c7e81535341fbf34b8b37e19a074ee37f0217
jay <jaypifiverr@gmail.com>
2023-05-29 20:25:15 -0400

    Page executes loading until requests are completed

--- client/src/components/select-routine/SelectRoutine.js
+++ client/src/components/select-routine/SelectRoutine.js
@@ -1,74 +1,162 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom'
 // import SelectExerciseWt from './SelectExerciseWt';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './SelectRoutine.css'
 
-function SelectRoutine( { userRoutine } ) {
+function SelectRoutine( { userRoutine, currentUser, setLoading, todaysRoutine } ) {
 
+    const history = useHistory()
+
+    const [ errors, setErrors ] = useState([])
+    const [ selectLast, setSelectLast ] = useState(false)
+    const [ prevRoutine, setPrevRoutine ] = useState([])
+    const getWeight = prevRoutine.map(rout => rout.exercise_rxes.map(ex => ex.weight))
+    
+    console.log(selectLast)
+    console.log(getWeight.length > 0)
+    console.log(prevRoutine)
+    
     const [ selectRoutine, setSelectRoutine ] = useState('')
     const [ routineArray, setRoutineArray ] = useState([])
-    const [ weightInput, setWeightInput ] = useState(0)
     const [ date, setDate ] = useState(new Date())
+    const [weightInput, setWeightInput] = useState(() => {
+        return routineArray.map(() => ({weight : ""})
+        )
+    })
 
+    console.log(todaysRoutine)
+
+    
+    function selectLastBox() {
+        setSelectLast(selectLast => !selectLast)
+    }
+
+    console.log(selectRoutine)
+
+    // const findExercise = todaysRoutine.find(ex => ex.exercise_routine.routine.name)
+// 
+    // console.log(findExercise.exercise_routine.routine.id === parseInt(selectRoutine))
 
     useEffect(() => {
+        if (selectLast) {
+            fetch(`/last_workout/?id=${selectRoutine}`)
+            .then(res => res.json())
+            .then(res => setPrevRoutine(res))
+        }
+    }, [selectLast])
+
+    useEffect(() => {
+        // if (findExercise.exercise_routine.routine.id === parseInt(selectRoutine)) {
+        //     alert('There is already an exercise routine planned for today.  You will need to delete the routine, then create a new one')
+        // } else {
         fetch(`/exercise_routines/?routine_id=${selectRoutine}`)
         .then(res => res.json())
         .then(res => setRoutineArray(res))
-    }, [selectRoutine])
+    // }
+}, [selectRoutine])
 
     function handleSelect(e) {
         setSelectRoutine(e)
     }
 
-    console.log(selectRoutine)
+    function handleChange(e, index) {
+        const {name, value} = e.target;
+        const list = [...weightInput];
+        list[index] = { ...list[index], [name]: parseInt(value, 10) };
+        setWeightInput(list)
+    }
+
+    // console.log(selectRoutine.length)
     console.log(routineArray)
+    // console.log(weightInput)
 
     function handleSubmit(e) {
         e.preventDefault();
-        {routineArray.map(rout => 
-            fetch('/exercise_rxs', {
-                method: "POST",
-                headers: {"Content-Type" : "application/json"},
-                body: JSON.stringify({
-                    exercise_routine_id: rout.id,
-                    weight: rout.weight,
-                    date: date,
-                    intensity: "",
-                    completed: "",
-                    percent_done: ""
-                })
-            })
-            .then(res => res.json())
-            .then(res => console.log(res))
-        )}
-    }
+        setLoading(true);
+        
+        const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+        const options = { timeZone: 'UTC', month: 'short', day: 'numeric', year: 'numeric' };
+        const formattedDate = date.toLocaleDateString('en-US', options);
+      
+        const promises = routineArray.map((rout, i) => {
+          return delay(1000 * i).then(() => {
+            const routine = weightInput[i].weight;
+      
+            return fetch('/exercise_rxes', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                exercise_routine_id: rout.id,
+                weight: routine,
+                date: formattedDate,
+                intensity: '',
+                percent_completed: '',
+                completed: ''
+              }),
+            });
+          });
+        });
+      
+        Promise.all(promises)
+          .then((responses) => {
+            const errorResponses = responses.filter((res) => !res.ok);
+            if (errorResponses.length === 0) {
+              setLoading(false);
+              history.push(`/users/${currentUser.username}`);
+            } else {
+              return Promise.all(errorResponses.map((res) => res.json()));
+            }
+          })
+          .then((errors) => {
+            if (errors) {
+              setLoading(false);
+              setErrors(errors);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            setLoading(false);
+          });
+      }
 
     return(
-        <form className='select-routine'>
+        <form className='select-routine' onSubmit={handleSubmit}>
             <div className='user-routine-container'>
-                <div className='routine-label-container flex'>
-                    <h3>Select Routine:</h3>
-                    <select onChange={(e) => handleSelect(e.target.value)}>
-                        <option value=""></option>
-                        {userRoutine.map(rout => 
-                        <option value={rout.id} key={rout.id}>{rout.name}</option>)}
-                    </select>
-                </div>
+                <div className='select-routine-label-container r-c'>
+                    <div className={`routine-label flex ${selectRoutine.length < 1 ? 'w100' : ''}`}>
+                        <h3>Select Routine:</h3>
+                        <select onChange={(e) => handleSelect(e.target.value)}>
+                            <option value=""></option>
+                            {userRoutine.map(rout => 
+                            <option value={rout.id} key={rout.id}>{rout.name}</option>)}
+                        </select>
+                    </div>
+                {selectRoutine.length > 0 ?
+                    <div className='checkbox-container c-c'>
+                        <div className='check-container flex'>
+                            <input type='checkbox' onChange={selectLastBox}/><h4> Weight from last exercise?</h4>
+                        </div>
+                        <div className='check-container flex'>
+                            <input type='checkbox'/><h4> Select earlier exercise</h4>
+                        </div>
+                    </div> : ""}
+                    </div>
             <div className='routine-exercises-container'>
-                {routineArray.map(rout =>
+                {routineArray.map((rout, index) =>
                 <>
-                <div className='select-exercise-label flex'>
+                <div className='select-exercise-label flex' key={rout.id}>
                     <h3>Exercise: {rout.workout.name} ({rout.workout.kind})</h3>
                 </div>
-                <div className='select-exercise-scheme flex' key={rout.id}>
-                    <div className='exercise-set-rep-counter flex'>
-                        <h4>Sets: {rout.sets}</h4>
-                    </div>
+                <div className='select-exercise-scheme flex' >
+
                     <div className='exercise-set-rep-counter flex'>
                         <h4>Weight:</h4>
-                        <input className='ex-set-counter' type='number' min='0' max='2000' step='5' value={""} onChange={(e) => setWeightInput(e.target.value)}/>
+                        <input className='ex-set-counter' type='number' name='weight' min='0' max='2000' step='5' value={selectLast ? parseInt(getWeight[index], 10) : weightInput.weight} onChange={(e) => handleChange(e, index)}/>
+                    </div>
+                    <div className='exercise-set-rep-counter flex'>
+                        <h4>Sets: {rout.sets}</h4>
                     </div>
                     <div className='exercise-set-rep-counter flex'>
                         <h4>Reps: {rout.reps}</h4>
@@ -86,9 +174,10 @@ function SelectRoutine( { userRoutine } ) {
                 </div>
                 <div className='date-picker-dropdown flex'>
                     <DatePicker 
+                    // value={new Date()}
                     name='exercise-date'
-                    // onChange={}
-                    selected={new Date()}
+                    onChange={(e) => setDate(e)}
+                    selected={date}
                     minDate={new Date()}
                 />
                 </div>
