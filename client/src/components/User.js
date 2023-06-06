0aeadd1b6fd3e9cf1a3228b790db81e8329e1460
jay <jaypifiverr@gmail.com>
2023-06-05 23:45:07 -0400

    User can update weight weekly

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,10 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import './User.css'
 
-function User( { updateUser, userRoutine, setUserRoutine, currentUser } ) {
+function User( { updateUser, userRoutine, setLoading, date, setUserRoutine, formattedDate, currentUser, setTodaysRoutine, todaysRoutine } ) {
+    
+    const last_weight = currentUser.weights.map(wt => { return ((wt.weight))})
+    const last_converted = (Number(last_weight[last_weight.length - 1]))
 
+    const userSignUp = new Date(currentUser.created_at);
+    const timeDifference = Math.abs(userSignUp.getTime() - date.getTime())
+    const timeDiff = Math.ceil(timeDifference / (1000 * 60 * 60 * 24))
+    
+    const [errors, setErrors] = useState([])
+    const [ userWeight, setUserWeight ] = useState(last_converted)
+    const [ updateWeight, setUpdateWeight ] = useState(false)
+    const [ updateEnabled, setUpdateEnabled ] = useState(false)
+    const hasOne = userRoutine.length > 0 
+    const hasOneAndTodaysRoutine = todaysRoutine.length > 0
+    
     const history = useHistory()
+    
+        useEffect(() => {
+            if (timeDiff === 7) {
+                setUpdateEnabled(true)
+            }
+        }, [])
 
     function handleLogout() {
         fetch('/logout', {
@@ -20,34 +40,147 @@ function User( { updateUser, userRoutine, setUserRoutine, currentUser } ) {
 
     useEffect(() => {
         if (currentUser !== false) {
-        fetch(`/routines/?user_id=${currentUser.id}`)
-        .then(res => res.json())
-        .then(res => setUserRoutine(res))
+        fetch(`/routine_names/?user_id=${currentUser.id}`)
+        .then(res => {
+            if (res.ok) {
+                res.json().then(res => {
+                    (setUserRoutine(res))
+                })
+            } 
+        })
+        .catch(error => {
+            console.log(error)
+        })    
         }
       }, [])
 
+      useEffect(() => {
+        if (currentUser !== false) {
+        setLoading(true)
+        fetch(`/last_routine/?date=${formattedDate}`)
+        .then(res => {
+            if (res.ok) {
+                res.json().then(res => {
+                    (setTodaysRoutine(res))
+                    setLoading(false)
+                    })
+                }
+            })
+                .then(data => {
+                    if (data) {
+                        setLoading(false)
+                        setErrors(data.errors)
+                    }
+                })
+                .catch(error => {
+                    setLoading(false)
+                    console.log(error)
+                })
+            }
+        }, [hasOneAndTodaysRoutine])
 
-    const hasOne = userRoutine.length > 0 ? true : false
+    function toggleUpdateWeight() {
+        setUpdateWeight(updateWeight => !updateWeight)
+    }
 
-    console.log(currentUser)
-    console.log(userRoutine)
-    console.log(hasOne)
+    function handleUpdateWtChange(e) {
+        setUserWeight(e.target.value)
+    }
+
+    function handleWeightChange() {
+        fetch('/weights', {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({
+                weight: userWeight,
+                user_id: currentUser.id
+            })
+        })
+        .then(res => {
+            if (res.ok) {
+                setUpdateWeight(false)
+                setUpdateEnabled(false)
+                return res.json()
+            } else {
+                return res.json()
+            }
+        })
+        .then((data) => {
+            if (data) {
+                setUpdateEnabled(false)
+                setUpdateWeight(false)
+                console.log(data.error)
+            }
+        })
+    }
+
+
+
     return(
         <div className="user c-c">
-                <button onClick={handleLogout}>Logout</button>
-            <div className='userpg-container c-c'>
-                <Link to='/start-exercise'>
-                    <button className='userpg-btn hm-btn'>Start Exercise</button>
-                </Link>
-                <Link to='/select-routine'>
-                    <button className={`userpg-btn ${hasOne ? "" : "not-allowed"}`}>{hasOne ? 'Select Exercise Routine' : 'Start Below'}</button>
-                </Link>
-                <Link to='/create-new'>
-                    <button className='userpg-btn hm-btn'>Create Exercise Routine</button>
-                </Link>
-                <Link to='/progress'>
-                    <button className='userpg-btn hm-btn'>View Progression</button>
-                </Link>
+            <div className='user-header wd flex'>
+                    <h4>Some Exercise App</h4>
+                    <p>Date: {formattedDate}</p>
+                    <button className='btn' onClick={handleLogout}>Logout</button>
+            </div>
+            <div className='userpg-containerr flex'>
+                <div className='userpg-info-container wd flex'>
+                    <div className='userpg-img ht c-c'>
+                        <p className='c-c'>img</p>
+                    </div>
+                    <div className='userpg-info ht flex'>
+                        <h4>Name:</h4>
+                        <h4>Height:</h4>
+                        <h4>Weight:</h4>
+                    </div>
+                    <div className='userpg-stat ht'>
+                        <p>{currentUser.name}</p>
+                        <p>{currentUser.feet} ft.</p><p>{currentUser.inches} in.</p>
+                        {/*update weight when a week has passed:*/}
+                        {updateWeight ?
+                            <div className='flex'>
+                                <input className='input-wt' type="number" step='0.1' value={userWeight} onChange={(e) => handleUpdateWtChange(e)}/>
+                                <button onClick={handleWeightChange}>Update</button>
+                            </div>
+                            :
+                                <>
+                            <div className='flex'>
+                                <div className='user-weight'>
+                                    <p>{userWeight} lbs.</p>
+                                </div>
+                            {updateEnabled ?
+                                <div className='user-update-link c-c'>
+                                    <p className='user-link' onClick={toggleUpdateWeight}>update weight</p>
+                                    {errors ? 
+                                    <div className='error-container'>
+                                        {errors.weight}
+                                        </div>
+                                    : ""}
+                                </div>
+                                : 
+                                ""}
+                            </div>
+                            </>
+                        }
+                    </div>
+                </div>
+                <div className='userpg-container wd c-c'>
+                    <Link to={hasOneAndTodaysRoutine & hasOne ? '/start-exercise' : "#"}>
+                        <button className={`userpg-btn hm-btn ${hasOneAndTodaysRoutine & hasOne ? "" : "not-allowed"}`}>{hasOneAndTodaysRoutine & hasOne ? 'BEGIN EXERCISE' : 'Create Routine Below'}</button>
+                    </Link>
+                    <Link to={hasOne ? hasOneAndTodaysRoutine ? "#" : '/select-routine' : '#'}>
+                        <button className={`userpg-btn ${hasOne ? hasOneAndTodaysRoutine ? "not-allowed" : "" : "not-allowed"}`}>{hasOne ? 'Select Exercise Routine' : 'Create Routine Below'}</button>
+                    </Link>
+                    <Link to='/create-new'>
+                        <button className='userpg-btn hm-btn'>Create Exercise Routine</button>
+                    </Link>
+                    <Link to={hasOne ? '/progress' : "#"}>
+                        <button className={`userpg-btn hm-btn ${hasOne ? "" : "not-allowed"}`}>View Progression</button>
+                    </Link>
+                    <Link to={hasOne ? '/manage'  : "#"}>
+                        <button className={`userpg-btn hm-btn ${hasOne ? "" : "not-allowed"}`}>Manage Routines</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
